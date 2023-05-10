af0794c34601e8f9ab3ad24a686e90f688f93e9c
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-05-10 13:43:15 -0700

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 number_of_subscribers(subreddit)
 
@@ -13,15 +14,32 @@ Returns:
 Raises:
   ValueError: If the subreddit is not valid.
 """
-import requests
+
 from fake_useragent import UserAgent
-ua = UserAgent()
+import requests
+
 
 def number_of_subscribers(subreddit):
-    response = requests.get('https://api.reddit.com/r/{}/about.json'.format(subreddit), allow_redirects=False, headers={
-                             "User-Agent": ua.random})
+    # Check if the subreddit is valid.
+    if not subreddit.startswith("r/"):
+        raise ValueError("The subreddit must start with 'r/'.")
+
+    # Make a request to the Reddit API.
+    response = requests.get(
+        'https://api.reddit.com/r/{}/about.json'.format(subreddit),
+        allow_redirects=False,
+        headers={
+            "User-Agent": UserAgent().random
+        }
+    )
+
+    # Check the response status code.
     if response.status_code == 200:
+        # Decode the JSON response.
         data = response.json()
+
+        # Return the number of subscribers.
         return data["data"]["subscribers"]
     else:
+        # Return 0 if the request failed.
         return 0
