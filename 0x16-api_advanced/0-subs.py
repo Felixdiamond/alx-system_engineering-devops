822b6ce5b091a52d897a702b16697b790dcfa30e
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-05-10 12:57:37 -0700

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,9 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    response = requests.get('https://api.reddit.com/r/{}/about.json'.format(subreddit))
+    response = requests.get('https://api.reddit.com/r/{}/about'.format(subreddit))
     if response.status_code == 200:
         data = response.json()
-        return data['subscribers']
+        return data
     else:
         return 0
