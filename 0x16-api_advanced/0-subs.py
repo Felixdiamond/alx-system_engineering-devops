cfbd1fbbd58f9d45f8cf2deef976f9b13cfd3a2a
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-05-10 13:00:59 -0700

    Access response properly

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 import requests
 
 def number_of_subscribers(subreddit):
-    response = requests.get('https://api.reddit.com/r/{}/about'.format(subreddit))
+    response = requests.get('https://api.reddit.com/r/{}/about.json'.format(subreddit))
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
