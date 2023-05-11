bdcf9f0a6f1a94b92027c42304848d8a8e9c0a51
Felixdiamond <diamondfelix006@gmail.com>
2023-05-11 03:18:31 -0700

    Format code to pycodestyle

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,8 +5,8 @@ import requests
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
     """
-       Queries the Reddit API, parses the title of all hot articles, and prints a 
-       sorted count of given keywords (case-insensitive, delimited by spaces. 
+       Queries the Reddit API, parses the title of all hot articles, and prints a
+       sorted count of given keywords (case-insensitive, delimited by spaces.
        Javascript should count as javascript, but java should not).
 
        Args:
