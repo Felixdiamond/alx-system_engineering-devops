70f2ed9f03fb4ad257d5b14a04dea1ebdb3ceaea
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-05-11 03:07:59 -0700

    Fix minor issues

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,29 +13,19 @@
 
 import requests
 
-
 def top_ten(subreddit):
-
-    # Check if the subreddit is valid.
-    if not subreddit:
-        print("Please provide a valid subreddit name.")
+    """prints the titles of the first 10 hot posts listed"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "Ayanokoji/2.1"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-
-    # Make a request to the Reddit API.
-    response = requests.get(
-        "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
-        allow_redirects=False,
-        headers={
-            "User-Agent": "Ayanokoji/2.1"})
-
-    # Check if the request was successful.
-    if response.status_code != 200:
-        print("Error: {} {}".format(response.status_code, response.reason))
-        return
-
-    # Parse the response data.
-    data = response.json()
-
-    # Print the titles of the first 10 hot posts.
-    for post in data["data"]["children"]:
-        print(post["data"]["title"])
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
