9aebaebbcdaba06988357777ae520154b7bddea2
Felixdiamond <diamondfelix006@gmail.com>
2023-05-11 01:42:20 -0700

    Format code to pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,37 @@
 import requests
 
+
 def top_ten(subreddit):
-  """
-  Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
 
-  Args:
-    subreddit: The name of the subreddit to query.
+    Args:
+      subreddit: The name of the subreddit to query.
 
-  Returns:
-    None.
-  """
+    Returns:
+      None.
+    """
 
-  # Check if the subreddit is valid.
-  if not subreddit:
-    print("Please provide a valid subreddit name.")
-    return
+    # Check if the subreddit is valid.
+    if not subreddit:
+        print("Please provide a valid subreddit name.")
+        return
 
-  # Make a request to the Reddit API.
-  response = requests.get("https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit), allow_redirects=False, headers={"User-Agent": "Ayanokoji/2.1"})
+    # Make a request to the Reddit API.
+    response = requests.get(
+        "https://api.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        allow_redirects=False,
+        headers={
+            "User-Agent": "Ayanokoji/2.1"})
 
-  # Check if the request was successful.
-  if response.status_code != 200:
-    print("Error: {} {}".format(response.status_code, response.reason))
-    return
+    # Check if the request was successful.
+    if response.status_code != 200:
+        print("Error: {} {}".format(response.status_code, response.reason))
+        return
 
-  # Parse the response data.
-  data = response.json()
+    # Parse the response data.
+    data = response.json()
 
-  # Print the titles of the first 10 hot posts.
-  for post in data["data"]["children"]:
-    print(post["data"]["title"])
+    # Print the titles of the first 10 hot posts.
+    for post in data["data"]["children"]:
+        print(post["data"]["title"])
