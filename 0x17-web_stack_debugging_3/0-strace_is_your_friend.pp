1818bda99a40361dd5ddf1b9921aebb8ad487e3c
Felixdiamond <diamondfelix006@gmail.com>
2023-05-13 01:43:10 -0700

    kamui

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# This will replace all instances of "phpp" with "php" in the
+# wp-settings.php file.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
