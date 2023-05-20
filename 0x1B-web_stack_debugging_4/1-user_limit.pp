c54f35b858d4407dec99364627242a927997573f
Felixdiamond <diamondfelix006@gmail.com>
2023-05-20 02:46:48 -0700

    kamui

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enables user 'holberton' to login and open files without error.
+
+# Increases the hard file limit for user 'Holberton'.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increases the soft file limit for user 'Holberton'.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
